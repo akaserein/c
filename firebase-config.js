903c32bde085c6ea7a1f930a52fec36d6573d6c4
akaserein <himojoit1234@gmail.com>
2025-09-24 17:15:51 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// Replace the values below with your project's config if different.
+export const firebaseConfig = {
+  apiKey: "AIzaSyBAOwLjCgopfBvkgXbTbCEKb7sU5VQ-JHU",
+  authDomain: "boxi-cm.firebaseapp.com",
+  databaseURL: "https://boxi-cm-default-rtdb.firebaseio.com",
+  projectId: "boxi-cm",
+  storageBucket: "boxi-cm.firebasestorage.app",
+  messagingSenderId: "7208548687",
+  appId: "1:7208548687:web:e38cdb5af7c7206f506d3a",
+  measurementId: "G-BE6R2X39BS"
+};
